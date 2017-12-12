12dcbef290e51d1ab13cd205b985f5d3f70fa572
jtzhang13 <jtzhang13@gmail.com>
2017-12-11 23:23:46 -0500

    1st working fgh without remapping

---
+++ quantum/constants.py
@@ -0,0 +1,34 @@
+
+import math as m
+
+# units
+# temperature
+nK = 1e-9
+uK = 1e-6
+mK = 1e-3
+# length
+ang = 1e-10
+nm = 1e-9
+um = 1e-6
+mm = 1e-3
+cm = 1e-2
+
+# atomic units
+amu = 1.660539040 * 1e-27 # [kg]
+a0 = 0.52917720859 * 1e-10 # bohr radius [m]
+
+e = 1.602176487 * 1e-19 # electron charge [C]
+
+eps0 = 8.854187817 * 1e-12 # vacuum permittivity [F/m]
+mu0 = 4 * m.pi * 1e-7 # vacuum permeability [N/A^2]
+c = 299792458 # speed of light [m/s]
+
+kB = 1.38064852 * 1e-23 # boltzmann constant [m^2 kg / s^2 K]
+
+hbar = 1.054571628 * 1e-34 # planck's constant [m^2 kg/s]
+h = 2 * m.pi * hbar # reduced planck's constant [m^2 kg/s]
+
+g = 9.80665 # gravitational acceleration [m/s^2]
+
+m_cs = 2.20694657 * 1e-25 # [kg]
+m_na = 0.381754035 * 1e-25 # [kg]
